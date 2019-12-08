466d1383eeae0832f5599d722ea4ca4db24d6135
Akash Vishwakarma <14cse031giet@gmail.com>
2019-12-08 19:48:27 +0530

    Updates

---
+++ precache-manifest.07c523de99a0c7a09f5e6799f47f45b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ec000f114f69882ef2f58858c9621e",
+    "url": "/Todo-Applicaton-Using-Hooks/index.html"
+  },
+  {
+    "revision": "7020b038db2eb098df57",
+    "url": "/Todo-Applicaton-Using-Hooks/static/css/main.aba2f912.chunk.css"
+  },
+  {
+    "revision": "7935fcbb7537a8ed1c59",
+    "url": "/Todo-Applicaton-Using-Hooks/static/js/2.0be5ddc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-Applicaton-Using-Hooks/static/js/2.0be5ddc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7020b038db2eb098df57",
+    "url": "/Todo-Applicaton-Using-Hooks/static/js/main.c9552415.chunk.js"
+  },
+  {
+    "revision": "123b0e1d254c0f5fdf55",
+    "url": "/Todo-Applicaton-Using-Hooks/static/js/runtime-main.56c8fcb7.js"
+  }
+]);
